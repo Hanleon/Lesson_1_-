28ffc893b6bf91cae1cd6b089409acf06ec44067
Hanleon <hanjiaqileon@gmail.com>
2020-05-02 10:34:21 +0800

    Add files via upload

---
+++ Post_test/Post_test/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Post_test
+{
+    public partial class Form1 : Form
+    {
+        private POST _POST = new POST();
+        private string url_1 = "https://class.campus-app.net/tcloud_class/activity/user/schedule-dot?RequestId=1587887167454";
+        private SortedDictionary<string, string> request_1 = new SortedDictionary<string, string> { { "ActivityId", "bp76iob6fdggt5eog93g" }, { "Grade", "Grade12" } };
+
+        private string url_2 = "https://class.campus-app.net/tcloud_class/activity/user/schedule?RequestId=1587887167454";
+        private SortedDictionary<string, string> request_2 = new SortedDictionary<string, string> { { "ActivityId", "bp76iob6fdggt5eog93g" }, { "Grade", "Grade12" },{ "Date","20200423" } };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = _POST.POST_JSON(url_1,_POST.Json2String(request_1));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = _POST.POST_JSON(url_2, _POST.Json2String(request_2));
+        }
+    }
+}
---
+++ Post_test/Post_test/POST.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post_test
+{
+    class POST
+    {
+        public string POST_JSON(string url, string jsonParam)
+        {
+            //发送请求
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/json;charset=UTF-8";
+            var byteData = Encoding.UTF8.GetBytes(jsonParam);
+            var length = byteData.Length;
+            request.ContentLength = length;
+            var writer = request.GetRequestStream();
+            writer.Write(byteData, 0, length);
+            writer.Close();
+
+            //接收数据
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")).ReadToEnd();
+
+            return responseString;
+        }
+
+        public string Json2String(SortedDictionary<string, string> dic)
+        {
+            string str = "{";
+            for (int i = 0; i < dic.Count; i++)
+            {
+                str += "\"" + dic.ToList()[i].Key + "\":\"" + dic.ToList()[i].Value + "\"";
+                if (i != dic.Count - 1) str += ",";
+            }
+            str += "}";
+            return str;
+        }
+    }
+}
