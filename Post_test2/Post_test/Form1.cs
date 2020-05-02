330c64bb03c35fbdecac00d32af1da7c440c0680
Hanleon <hanjiaqileon@gmail.com>
2020-05-02 10:36:20 +0800

    Add files via upload

---
+++ Post_test2/Post_test/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Post_test
+{
+    public partial class Form1 : Form
+    {
+        private POST _POST = new POST();
+        private string url_1 = "https://class.campus-app.net/tcloud_class/activity/user/schedule-dot?RequestId=1587887167454";
+        private SortedDictionary<string, string> request_1 = new SortedDictionary<string, string> { { "ActivityId", "bp76iob6fdggt5eog93g" }, { "Grade", "Grade12" } };
+
+        private string url_2 = "https://class.campus-app.net/tcloud_class/activity/user/schedule?RequestId=1587887167454";
+        private SortedDictionary<string, string> request_2 = new SortedDictionary<string, string> { { "ActivityId", "bp76iob6fdggt5eog93g" }, { "Grade", "Grade12" },{ "Date","20200423" } };
+
+
+        private SplitData _split = new SplitData();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result_str = _POST.POST_JSON(url_1,_POST.Json2String(request_1));
+            string[] result_arr = _split.Split_1(result_str);
+
+            string result = "";
+            for (int i = 0; i < result_arr.Length; i++) result += result_arr[i] + "\r\n";
+            textBox1.Text = result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string result_str = _POST.POST_JSON(url_2, _POST.Json2String(request_2));
+            string[] result_arr = _split.Split_2(result_str);
+
+            string result = "";
+            for (int i = 0; i < result_arr.Length; i++) result += result_arr[i] + "\r\n";
+            textBox1.Text = result;
+        }
+    }
+}
---
+++ Post_test2/Post_test/SplitData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Post_test
+{
+    class SplitData
+    {
+        public string[] Split_1(string data)
+        {
+            string[] result = null;
+            string new_str = CutOutString(data,"[", "]");
+            new_str = new_str.Replace("\"","");
+            result = new_str.Split(',');
+            return result;
+        }
+
+        public string[] Split_2(string data)
+        {
+            string[] result = null;
+            string[] str_arr = data.Split('\"');
+            string result_str = "";
+            for (int i = 0; i < str_arr.Length; i++)
+            {
+                if (str_arr[i] == "Name" || str_arr[i] == "ResourcesName" || str_arr[i] == "ResourcesUrl") result_str += str_arr[i + 2] + "\n";
+            }
+            if (result_str != "")
+            {
+                result_str = result_str.Substring(0, result_str.Length - 1);
+                result_str = result_str.Replace("\\u0026","&");
+                result = result_str.Split('\n');
+            }
+            return result;
+        }
+
+        private string CutOutString(string sourse, string startstr, string endstr)
+        {
+            Regex rg = new Regex("(?i)(?<=\\"+ startstr + ")(.*)(?=\\"+endstr+")");
+            return rg.Match(sourse).Value;
+        }
+    }
+}
